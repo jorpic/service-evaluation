1c4cdf09431fb68bb698a05ce8a97888b7c91a46
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-09-26 02:05:05 +0300

    add Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import {h} from 'preact'
 import {F} from './types'
+import {Form} from './components/Form'
 
 export const App: F<any> = () => {
   return (
     <section class='app'>
+      <div class='container'>
+        <Form/>
+      </div>
     </section>
   )
 }
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,8 @@
+import {h} from 'preact'
+
+export const Form = () => {
+  return (
+    <form class='form'>
+    </form>
+  )
+}
