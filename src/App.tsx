16ddef2ece23631b12293b912f739cda236329bc
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-09-30 10:48:17 +0300

    add Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import {h} from 'preact'
 import {F} from './types'
 import {Form} from './components/Form'
+import {Title} from './components/Title'
 
 export const App: F<any> = () => {
   return (
     <section class='app'>
       <div class='container'>
         <Form/>
+        <div className="app__title">
+          <Title text='Опрос об удовлетворенности клиентов'/>
+        </div>
       </div>
     </section>
   )
