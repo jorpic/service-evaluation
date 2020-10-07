010844c042c7a63a1fb1f02d9a82f48a9a50e5ae
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-10-07 12:25:52 +0300

    add state isLoading for  to send a survey, add button and progress bar

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,19 +1,35 @@
 import {h} from 'preact'
-import {Fieldset} from '../Fieldset'
+import {useState} from 'preact/hooks'
+import cn from 'classnames'
+import {Question} from '../Question'
+import {Progress} from '../Progress'
 import {F, Data} from '../../types'
 import './index.scss'
 
 type Props = {
   questions: Data
+  size: number
   onChange: (i: string) => void
-  typeProgress: Set<string>
+  onDone: (i: boolean) => void
 }
 
-export const Form: F<Props> = ({onChange, questions, typeProgress}) => {
+export const Form: F<Props> = ({onChange, questions, onDone, size}) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const setDone = () => {
+    setIsLoading(true)
+    setTimeout(() => onDone(true), 2000)
+  }
   return (
     <form class='form' onSubmit={e => e.preventDefault()}>
-      {questions && questions.map(r => <Fieldset answers={r.answers} question={r.question} type={r.type} onChange={onChange} typeProgress={typeProgress}/>)}
-
+      <div class='form__questions'>
+        {questions && questions.map(q => <Question answers={q.answers} question={q.question} type={q.type} onChange={onChange}/>)}
+      </div>
+      <div class='form__btn'>
+        <button class={cn('button is-primary', {'is-loading': isLoading})} onClick={setDone} type='submit' name='submit'>Отправить</button>
+      </div>
+      <div class='form__progress'>
+        <Progress progress={size} length={questions.length}/>
+      </div>
     </form>
   )
 }
