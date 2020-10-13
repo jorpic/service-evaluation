931bfb01ca73085816713e34353c5a310525dfe4
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-10-13 17:01:18 +0300

    drop unneeded func and property

--- src/components/Answer/index.tsx
+++ src/components/Answer/index.tsx
@@ -1,29 +1,16 @@
 import {h} from 'preact'
 import {F} from '../../types'
 import './index.scss'
-import {useState} from "preact/hooks";
 
 type Props = {
   type: string
-  name: string
   text: string
-  onChange: (i: string) => void
-  setCheckboxChecked: (i: boolean) => void
 }
 
-export const Answer: F<Props> = ({type, name, text, onChange, setCheckboxChecked}) => {
-
-  const setCheckedName = (e: any) => {
-    e.target.checked && onChange(name)
-    type === 'checkbox' && e.target.checked && setCheckboxChecked(true)
-  }
-
-  return (
+export const Answer: F<Props> = ({type, text}) =>
     <span class='form__answer control'>
-      <label class={`${type} form__answer-label`}>
-        <input class={type} type={type} name={name} onChange={setCheckedName}/>
+      <label class={`${type} form__answer-label`} id={text}>
+        <input class={type} id={text} type={type} name={type}/>
         <span class='form__answer-label__text'>{text}</span>
       </label>
     </span>
-  )
-}
