8228fd3638730d224f9ceb0cbc17307e2b024e4a
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-10-13 17:05:08 +0300

    drop map answers, add map ratingStars on display Star, add SelectedRating, IconTitle

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,30 +1,68 @@
 import {h} from 'preact'
+import {useState} from 'preact/hooks'
+import {Star} from '../Star'
 import {Answer} from '../Answer'
-import {F, Answers} from '../../types'
+import {F} from '../../types'
 import './index.scss'
-import {useState} from "preact/hooks";
 
 type Props = {
-  answers: Answers
   question: string
-  type: string
-  onChange: (i: string) => void
+  star: any
+  onClick: (i: string) => void
+  onSelectedStar: (i: string) => void
 }
 
-export const Question: F<Props> = ({answers, question, type, onChange}) => {
-  const [checkboxChecked, setCheckboxChecked] = useState(false)
+export const Question: F<Props> = ({question, star, onClick, onSelectedStar}) => {
+  const ratingStars = [
+    'veryGood',
+    'good',
+    'normal',
+    'bad',
+    'veryBad'
+  ]
+
   return (
     <fieldset class='form__block'>
-      <h4 class='form__block-text'>
+      <h2 class='form__block-text'>
         {question}
-      </h4>
-      <div class='form__block-data'>
-        {answers.map(({text, name}) =>
-          <Answer type={type} text={text} name={name} onChange={onChange} setCheckboxChecked={setCheckboxChecked}/>)}
-        <div class='form__block-data__btn'>
-          {type === 'checkbox' && checkboxChecked && <button>Ok</button>}
-        </div>
+      </h2>
+      <div class='form__block-stars'>
+        {ratingStars.map((star, i) => <Star i={i} star={star} onSelectedStar={onSelectedStar}/>)}
       </div>
+      {star && <div class='form__block-select'>
+        <SelectedRating star={star} onClick={onClick}/>
+      </div>}
     </fieldset>
+  )
+}
+
+const SelectedRating: F<any> = ({star, onClick}) => {
+  return (
+    <div class='selected-rating'>
+      <h3 class='selected-rating__title'>{star.title}</h3>
+      {star.icons &&
+      <div class='selected-rating__selected-star'>
+        {star.icons.map((star: any) => <IconTitle key={star.title} icon={star.icon} title={star.title} onClick={onClick}/>)}
+      </div>}
+      {star.answers && <div class='selected-rating__answers'>
+        {star.answers.map((answer: any) => <Answer key={answer} type='checkbox' text={answer}/>)}
+      </div>}
+    </div>
+  )
+}
+
+const IconTitle: F<any> = ({icon, title, onClick}) => {
+  const [display, setDisplay] = useState(false)
+  const setSelected = () => {
+    setDisplay(!display)
+    onClick(title)
+  }
+  return (
+    <div class='icon' style={!display && 'filter: grayscale(100%);'} onClick={setSelected}>
+      <div>
+        <img src={icon} alt={title}/>
+        <div class='icon__title'>{title}</div>
+      </div>
+    </div>
   )
 }
